6b12b0797f8b1bf3081abb1c908b8bb84271ccbb
meetdomadiya <meet.do1998@gmail.com>
2024-03-26 14:01:32 +0100

    choices can be modified and added generic views

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
 admin.site.register(Question) #make polls modifiable
+admin.site.register(Choice) #make polls modifiable
